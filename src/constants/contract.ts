55eb02fc6f50ae5dde68cb489272c4fc049f2a9f
OxBryte <silascyrax@gmail.com>
2025-12-29 20:28:32 +0100

    changed (src/constants/contract.ts)

--- src/constants/contract.ts
+++ src/constants/contract.ts
@@ -11,6 +11,22 @@ export const CONTRACT_CONFIG = {
   network: "mainnet",
 };
 
+/**
+ * Christmas Presents contract configuration
+ * Update these values after deploying the Christmas presents contract
+ */
+export const CHRISTMAS_PRESENTS_CONFIG = {
+  // Contract address - update this after deployment
+  contractAddress: "SP3YGA4JT7289RWGB9SDXTNRCN2FQCRVFSTRRPN4F", // TODO: Update with actual contract address
+  contractName: "christmas-present",
+  // Network: 'testnet' or 'mainnet'
+  network: "mainnet",
+};
+
 export const getContractIdentifier = () => {
   return `${CONTRACT_CONFIG.contractAddress}.${CONTRACT_CONFIG.contractName}`;
+};
+
+export const getChristmasPresentsContractIdentifier = () => {
+  return `${CHRISTMAS_PRESENTS_CONFIG.contractAddress}.${CHRISTMAS_PRESENTS_CONFIG.contractName}`;
 };
